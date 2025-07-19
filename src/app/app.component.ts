454ddf795e3cf65372e405de8fcfd7eb64c933bc
Blizz1800 <blizzsoftword@gmail.com>
2025-07-19 19:33:41 -0400

    i18n implemented with ngx-translate and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MenuComponent } from './components/menu/menu.component';
+import { TranslationService } from './common/i18n/translation.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  constructor(private translator: TranslationService) {}
 }
