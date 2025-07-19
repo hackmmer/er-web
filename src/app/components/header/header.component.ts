5c4c58e9fce8e8f2ed42479bc0a7d807459ed895
Syrus23 <syrus2309@gmail.com>
2025-07-19 14:40:51 -0400

    Started work in header component.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isMenuOpen:boolean = false;
+
+  toggleMobileMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.isMenuOpen = false;
+  }
+}
