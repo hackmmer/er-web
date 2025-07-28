9dd531d1ea62657fa5d12aa521c252c0ced6d5f6
Syrus23 <syrus2309@gmail.com>
2025-07-28 17:07:46 -0400

    Translations: Added english translation.

--- src/app/common/i18n/langs/en.ts
+++ src/app/common/i18n/langs/en.ts
@@ -1,6 +1,62 @@
 export const locale = {
   lang: 'en',
   locale: {
+    'title': "Er'Caliente",
+    home: {
+      'title': 'Sandwiches that ignite your senses',
+      'details': "At Er'Caliente we don't just serve sandwiches, we create experiences that will make you sweat. Fresh ingredients, explosive combinations, and a touch of spice you'll never forget.",
+    },
+    menu: {
+      'title': 'Our Menu',
+      'details': 'Sandwiches created to awaken your senses. Every bite is a new experience that will leave you wanting more.',
+      'ingredients': 'Ingredients'
+    },
+    not_found: {
+      'title': 'Oops! Looks like this sandwich got burnt on the grill',
+      'details': "It seems the page you're looking for has disappeared like the last sandwich at lunchtime. While our chefs investigate this culinary mystery, why not try one of our delicious options?",
+      'wrong': 'Something went wrong in the kitchen...',
+      'lost': 'Did you get lost looking for something spicy?',
+      'details_2': "At Er'Caliente, even our mistakes are tasty. While we fix this issue, enjoy our delights",
+      'not_found': 'PAGE NOT FOUND',
+    },
+    buttons: {
+      'add': 'Add',
+      'orders': 'Order Now!',
+      'menu': 'View Full Menu',
+      'cart': 'View Cart',
+      'profile': 'Profile',
+      'settings': 'Settings',
+      'logout': 'Logout',
+      'signIn': 'Sign In',
+      'signUp': 'Sign Up',
+    },
+    login: {
+      'enter': 'Login',
+      'register': "Register",
+      'firstname': 'First Name',
+      'lastname': 'Last Name',
+      'email': 'Email',
+      'password': 'Password',
+      'repassword': 'Confirm Password',
+      'haventaccount': 'Don\'t have an account yet?',
+      'forgetpassword': 'Forgot your password?',
 
+      /* ERRORS */
+      'err_firstname_req': 'First name is required.',
+      'err_lastname_req': 'Last name is required.',
+      'err_email_req': 'Email is required.',
+      'err_email_invalid': 'Email is invalid.',
+      'err_password_req': 'Password is required.',
+      'err_password_minlen': 'Password must be at least 6 characters.',
+      'err_repassword_match': 'Passwords do not match.',
+      'err_repassword_req': 'Password confirmation is required.'
+    },
+    others: {
+      'hot': 'Hot',
+      'too_hot': 'Too hot!',
+      'spicy': 'Very Spicy!',
+      'orders': 'Orders',
+      'subtotal': 'Subtotal'
+    }
   }
 }
