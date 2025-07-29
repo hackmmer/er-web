a7ce29a96531619d0981a66d66f5a8a8b5f105a4
Azure-dvl <subarachisekai@gmail.com>
2025-07-29 18:54:44 -0400

    footer completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { TranslationService } from './common/i18n/translation.service';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
