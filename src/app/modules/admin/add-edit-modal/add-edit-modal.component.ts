fa6f8ab294d210b6e0a3dee9f54cd27ad5b32605
Azure-dvl <subarachisekai@gmail.com>
2025-08-10 23:30:56 -0400

    vista responsive plus admin config and modal create

---
+++ src/app/modules/admin/add-edit-modal/add-edit-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-modal',
+  imports: [],
+  templateUrl: './add-edit-modal.component.html',
+  styleUrl: './add-edit-modal.component.css'
+})
+export class AddEditModalComponent {
+
+  isedit!:boolean
+
+  constructor(){
+    
+  }
+}
--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { IProduct } from '@models/product';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin',
-  imports: [TranslatePipe, RouterLink],
+  imports: [TranslatePipe, NgTemplateOutlet, NgIf],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  
+  sidebarOpen = false;
+  activeItem = 'products';
+  
+  // Definimos los templates como propiedades públicas
+  @ViewChild('productsTemplate', { static: true }) productsTemplate!: TemplateRef<any>;
+  @ViewChild('promosTemplate', { static: true }) promosTemplate!: TemplateRef<any>;
+  @ViewChild('ordersTemplate', { static: true }) ordersTemplate!: TemplateRef<any>;
+  @ViewChild('testimonialsTemplate', { static: true }) testimonialsTemplate!: TemplateRef<any>;
+  @ViewChild('usersTemplate', { static: true }) usersTemplate!: TemplateRef<any>;
+  @ViewChild('settingsTemplate', { static: true }) settingsTemplate!: TemplateRef<any>;
 
+  // Obtenemos el template actual basado en el ítem activo
+  get activeTemplate(): TemplateRef<any> | null {
+    switch (this.activeItem) {
+      case 'products': return this.productsTemplate;
+      case 'promos': return this.promosTemplate;
+      case 'orders': return this.ordersTemplate;
+      case 'testimonials': return this.testimonialsTemplate;
+      case 'users': return this.usersTemplate;
+      case 'settings': return this.settingsTemplate;
+      default: return null;
+    }
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  setActiveItem(itemId: string) {
+    this.activeItem = itemId;
+    if (this.sidebarOpen) {
+      this.sidebarOpen = false;
+    }
+  }
+
+  add_edit_product(product?:IProduct){
+    
+  }
 }
