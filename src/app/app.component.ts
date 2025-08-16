b2f92cf812c636b77e0fbb31bc6463a9de25dacf
Syrus23 <syrus2309@gmail.com>
2025-08-16 05:33:19 -0400

    Implemented LoadingService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { TranslationService } from './common/i18n/translation.service';
 import { HeaderComponent } from './components/header/header.component';
 
+import { LoadingService } from '@services/loading.service';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  constructor(private translator: TranslationService) {}
+  constructor(private translator: TranslationService, public loadingService: LoadingService) {}
 }
