2a912e3260e0f293e7a869740c8d8a43d30ced40
Blizz1800 <blizzsoftword@gmail.com>
2025-08-17 17:21:04 -0400

    width and height tags added

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -47,6 +47,8 @@ export class SEOService {
 
   updateOgImage(image: string) {
     this.meta.updateTag({ property: 'og:image', content: image });
+    this.meta.updateTag({ property: 'og:image:width', content: '1536' });
+    this.meta.updateTag({ property: 'og:image:height', content: '1024' });
     this.meta.updateTag({ name: 'twitter:image', content: image });
     this.meta.updateTag({
       name: 'twitter:card',
